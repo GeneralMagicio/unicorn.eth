32cf26edf586a11344d26696c670503f825dcd8f
Mahdi Ghajary <mahdighajary@gmail.com>
2024-05-06 14:41:56 +0330

    Add react swipeable

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -90,7 +90,7 @@ export default function Dashboard() {
           {priceFormatter.format(estimatedTotalValue)}
         </Typography>
       </BalanceBox>
-      {showPromotionBox && canMintPOAP && (
+      {true && (
         <PromotionBox
           title="Claim your digital collectible"
           subtitle="Welcome to your web3 wallet."
