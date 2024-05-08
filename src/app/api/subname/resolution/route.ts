0a8ba41af6e83ead7cf52a9cbc098b764cc7adb1
Youssef <yussdev@gmail.com>
2024-05-08 16:06:18 +0330

    feat(app): setup for fleek

--- src/app/api/subname/resolution/route.ts
+++
@@ -1,8 +0,0 @@
-import { getSubnameResolution } from '@/services/enService'
-
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url)
-  const address = searchParams.get('address') || ''
-  const data = await getSubnameResolution({ address })
-  return Response.json(data)
-}
