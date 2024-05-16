5d0933444e23d5021ebb0102e9e419396ded58b1
Youssef <yussdev@gmail.com>
2024-05-16 19:54:56 +0330

    fix(axios): fix base url

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const axiosInstance = axios.create({
   baseURL:
-    process.env.NEXT_PUBLIC_BASE_URL || 'https://unicorn.cupofjoy.store/',
+    process.env.NEXT_PUBLIC_BASE_URL || 'https://unicorn.melodicdays.shop/',
   headers: {
     'Content-type': 'application/json',
   },
