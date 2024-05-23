b4ab6a045b1dfd493413c3f7333baa925293a4cd
Mahdi <mahdighajary@gmail.com>
2024-05-23 14:18:40 +0330

    Remove smart wallet address from dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -70,9 +70,7 @@ export default function Dashboard() {
       </header>
       <BalanceBox>
         <Typography color="inherit" fontVariant="small">
-          {`Estimated Value for: ${
-            account?.address ? shortenEthereumAddress(account.address) : ''
-          }`}
+          Estimated Value:
         </Typography>
         <Typography color="text" fontVariant="extraLarge">
           {priceFormatter.format(estimatedTotalValue)}
